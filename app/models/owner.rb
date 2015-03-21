15095a2a4d5aa9999b28adecb60ffd126105f10c
Prelang Builder <builder@prelang.com>
2015-03-21 02:37:46 +0000

    Defined validation for attribute 'phone' on model 'Owner'

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,4 +2,5 @@ class Owner < ActiveRecord::Base
   has_many :events
   validates_formatting_of :zip, using: :us_zip
   validates_formatting_of :website, using: :url
+  validates_formatting_of :phone, using: :us_phone
 end
