f69ffbc78c83c5187031346009672606c87cdca1
Prelang Builder <builder@prelang.com>
2015-03-21 02:37:46 +0000

    Defined validation for attribute 'header_image' on model 'Owner'

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,4 +3,5 @@ class Owner < ActiveRecord::Base
   validates_formatting_of :zip, using: :us_zip
   validates_formatting_of :website, using: :url
   validates_formatting_of :phone, using: :us_phone
+  validates_formatting_of :header_image, using: :url
 end
